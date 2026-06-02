ef002910c20694d8b22d998025af55fd51abd210
jack <computadordb@@gmail.com>
2026-06-02 15:23:03 -0300

    mend

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: "https://hooper-land.vercel.app",
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://hooper-land.vercel.app",
+  //baseURL: "http://127.0.0.1:8000",
 
   timeout: 10000,
   headers: {
